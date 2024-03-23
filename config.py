3b8d743f6f83b1fc357fa06b6b18fa81c0b305c7
Redeyealone <154379249+Redeyealone@users.noreply.github.com>
2024-03-24 03:24:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,5 +6,5 @@ class Config:
     SUDO = list(int(i) for i in os.environ.get("SUDO", "6083498673").split(" "))
     START_IMG="https://te.legra.ph/file/735920504b44b3565242a.jpg"
     BOT_ID= "6437630510"
-    BOT_USERNAME="WAIFU_x_bot"
-    BOT_NAME="WAIFU_x_BOT"
+    BOT_USERNAME="WAIFU_x_robot"
+    BOT_NAME="WAIFU_x_robot"
