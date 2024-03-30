9887ca381d5eb31f37c236b6e28c07f3c6f5769f
Redeyealone <154379249+Redeyealone@users.noreply.github.com>
2024-03-31 04:21:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ class Config:
     TOKEN="6872064337:AAGyofMcK81eno1XLCTG2f4LeRf9FacB-5Y"
     SUDO = list(int(i) for i in os.environ.get("SUDO", "6661105054").split(" "))
     START_IMG="https://te.legra.ph/file/735920504b44b3565242a.jpg"
-    BOT_ID= "6708079317"
-    BOT_USERNAME="Raiden_manage_bot"
-    BOT_NAME="Raiden_manage_bot"
+    BOT_ID= "6872064337"
+    BOT_USERNAME="@Take_waifu_bot"
+    BOT_NAME="@Take_waifu_bot"
