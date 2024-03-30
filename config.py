b38e66a8f6038c94006a3a892c8dcece4a6398e6
Redeyealone <154379249+Redeyealone@users.noreply.github.com>
2024-03-30 21:22:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 class Config:
     API_ID="22981583"
     API_HASH="c02ca0834d5d92ffb31fea2b50060195"
-    TOKEN="6859073442:AAFFCb2qA648IETNkpgASW8h-gKSQZx-nZg"
+    TOKEN="6823915064:AAFahumOfg3gtgpNPety00nQDP6lWGlz_oc"
     SUDO = list(int(i) for i in os.environ.get("SUDO", "6083498673").split(" "))
     START_IMG="https://te.legra.ph/file/735920504b44b3565242a.jpg"
     BOT_ID= "6859073442"
