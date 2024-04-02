c3e7af24014f0da5883e769d3edeaf7aabf2a12e
Redeyealone <154379249+Redeyealone@users.noreply.github.com>
2024-04-02 10:17:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,9 +2,9 @@ import os
 class Config:
     API_ID="22981583"
     API_HASH="c02ca0834d5d92ffb31fea2b50060195"
-    TOKEN="6578807444:AAE1uxAWF4FXboGZSO1g62u-camY1_3I5mc"
-    SUDO = list(int(i) for i in os.environ.get("SUDO", "6083498673").split(" "))
+    TOKEN="7186575419:AAFlmoyTtJ8LpJQ2COFCxJLA292tEc8wC_4"
+    SUDO = list(int(i) for i in os.environ.get("SUDO", "5516379068").split(" "))
     START_IMG="https://te.legra.ph/file/735920504b44b3565242a.jpg"
-    BOT_ID= "6578807444"
-    BOT_USERNAME="@Take_waifu_bot"
-    BOT_NAME="@Take_waifu_bot"
+    BOT_ID= "7186575419"
+    BOT_USERNAME="Redeye_ban_all_bot"
+    BOT_NAME="Redeye_ban_all_bot"
